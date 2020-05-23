979bcb9e94c0cc68122d46e3bd9cbf6dd6a83498
M@x <ruitao.mu@gmail.com>
2020-05-23 16:18:30 +0800

    Build Maze in 2nd floor of AlgoTemple

--- scripts/MineAlgo.py
+++ scripts/MineAlgo.py
@@ -1,6 +1,7 @@
 from mcpi.minecraft import Minecraft
 import hanoi
 import sort
+import Temp2ndFloorMaze
 
 import time
 import random
@@ -13,10 +14,16 @@ sort.init(mc)
 while True:
     flagFinished = False
     while flagFinished == False:
+        pos = mc.player.getTilePos()
+        if Temp2ndFloorMaze.isInsideTemple2ndFloorMaze(pos) == True:
+            if mc.getBlock(pos.x, pos.y-1, pos.z) == 206: # End Stone Brick(id:end_bricks)
+                mc.setBlock(pos.x, pos.y-1, pos.z, 89)   # Glowstone
         blockEvents = mc.events.pollBlockHits()
         for blockEvent in blockEvents:
             print(blockEvent)
             if sort.onSortMachineEvent(mc, blockEvent) == True:
                 pass
             elif hanoi.onHanoiMachineEvent(mc, blockEvent) == True:
+                pass
+            elif Temp2ndFloorMaze.onTemp2ndFloorMazeEvent(mc, blockEvent) == True:
                 pass
