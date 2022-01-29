dcb40fbf82cbcbd4b0052b115d8619604b6dc279
M@x <ruitao.mu@gmail.com>
2022-01-29 11:18:01 +0800

    Add Constant.py to maps

---
+++ maps/AlgoDesert/constants.py
@@ -0,0 +1,12 @@
+templeBuildingPosition_x = 70
+templeBuildingPosition_y = 64
+templeBuildingPosition_z = 31
+
+
+templeBasePosition_x = templeBuildingPosition_x + 4
+templeBasePosition_y = templeBuildingPosition_y
+templeBasePosition_z = templeBuildingPosition_z
+
+templeLength_x = 57
+templeLength_z = 60
+templeHeight = 21
